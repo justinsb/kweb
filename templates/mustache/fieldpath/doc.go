77d1c9a79be8860dc5dc4463685bc08a1c25b37f
justinsb <justinsb@google.com>
2022-10-01 12:03:21 -0400

    Tweak comment to be a little more precise.

--- templates/mustache/fieldpath/doc.go
+++ templates/mustache/fieldpath/doc.go
@@ -2,7 +2,7 @@
 Package fieldpath implements a simple grammar for evaluating expressions,
 using an intuitive dot-based syntax (foo.spec.bar).
 
-It is a little more nil-tolerant than obvious alternatives.
+It is more nil-tolerant than obvious alternatives.
 */
 
 package fieldpath
