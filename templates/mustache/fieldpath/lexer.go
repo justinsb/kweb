82cfd587bf9fa73536344d165ee99ed4e9419b2c
justinsb <justinsb@google.com>
2024-01-07 19:50:42 -0500

    Allow numbers in identifiers (after the first character)

--- templates/mustache/fieldpath/lexer.go
+++ templates/mustache/fieldpath/lexer.go
@@ -66,6 +66,9 @@ runeLoop:
 		case '_':
 			s = append(s, r)
 
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			s = append(s, r)
+
 		default:
 			if ('a' <= r && r <= 'z') || ('A' <= r && r <= 'Z') {
 				s = append(s, r)
